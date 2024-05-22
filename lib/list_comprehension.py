45f96e1f9fbbcd99ae3e0d40d2b89f89fe56d0d0
Muny1re1 <munyiriandy@gmail.com>
2024-05-22 14:41:08 +0300

    labDone

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    even = [n for n in num_list if (n % 2) == 0]
+    print(even)
+    return even
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [sentence + '!' for sentence in sentence_list]
+    print(exclamation_list)
+    return exclamation_list
